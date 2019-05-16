7a420ec24ef3f80c592d302cfc01dff41d375ceb
Greg Smith <gregsmith446@gmail.com>
2019-05-16 14:05:19 -0400

    TDD Environment Set Up + Ran First Red, Green, Refactor

---
+++ FizzBuzzKata/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FizzBuzzKata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class FizzBuzzer
+    {
+        public string FizzBuzz(int input)
+        {
+            return "1";
+        }
+    }
+}
---
+++ FizzBuzzTests/FizzBuzzTests.cs
@@ -0,0 +1,23 @@
+﻿using FizzBuzzKata;
+using NUnit.Framework;
+
+namespace FizzBuzzTests
+{
+    [TestFixture]
+
+    public class FizzBuzzTests
+    {
+        [Test]
+        public void FizzBuzzer_GivenInt1_ReturnsString1()
+        {
+            int input = 1;
+            string expected = "1";
+
+            FizzBuzzer fb = new FizzBuzzer();
+            string actual = fb.FizzBuzz(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
+
